29a5607828529f4958e137b183bec1cf75cd891b
tamchan <contact.tamtrung@gmail.com>
2026-01-09 11:45:06 +0700

    Change file

--- A50812-A51090/DoiTuong.cpp
+++ A50812-A51090/DoiTuong.cpp
@@ -1,10 +1,12 @@
 #include "DoiTuong.h"
-DoiTuong::DoiTuong() : ma(" "), ten(" ") {}
+DoiTuong::DoiTuong() : ma(""), ten("") {}
 DoiTuong::DoiTuong(string ma, string ten) : ma(ma), ten(ten) {}
 DoiTuong::~DoiTuong() {}
-string DoiTuong::getMa() const {return ma;}
-string DoiTuong::getTen() const {return ten;}
 
-void DoiTuong::setMa(string& ma) {this->ma = ma;}
-void DoiTuong::setTen(string& ten) {this->ten = ten;}
-    
+string DoiTuong::layMa() const { return ma;}
+
+string DoiTuong::layTen() const { return ten;}
+
+void DoiTuong::datMa(string ma) { this->ma = ma;}
+
+void DoiTuong::datTen(string ten) { this->ten = ten;}
--- A50812-A51090/DoiTuong.h
+++ A50812-A51090/DoiTuong.h
@@ -1,28 +1,29 @@
 #ifndef DOITUONG_H
 #define DOITUONG_H
-#include <iostream>
-#include <fstream>
+
 #include <string>
+#include <fstream>
+#include <iostream>
 using namespace std;
 class DoiTuong {
-    protected:
+protected:
     string ma;
     string ten;
 
-    public:
+public:
     DoiTuong();
     DoiTuong(string ma, string ten);
     virtual ~DoiTuong();
-
+    
     virtual void nhapThongTin() = 0;
-    virtual void hienThiThongTin() = 0;
+    virtual void hienThiThongTin() const = 0;
     virtual void ghiFile(ofstream& file) const = 0;
     virtual void docFile(ifstream& file) = 0;
-
-    string getMa() const;
-    string getTen() const;
-    void setMa(string& ma);
-    void setTen(string& ten);
-
+    
+    string layMa() const;
+    string layTen() const;
+    void datMa(string ma);
+    void datTen(string ten);
 };
+
 #endif
