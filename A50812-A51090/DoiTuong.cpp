0989ceedbd4ccac66a0900d20da02e6dd4f7a15c
tamchan <contact.tamtrung@gmail.com>
2026-01-10 23:26:44 +0700

    change file doituong

--- A50812-A51090/DoiTuong.cpp
+++ A50812-A51090/DoiTuong.cpp
@@ -2,12 +2,18 @@
 
 DoiTuong::DoiTuong() {
     ma = "";
-    ten = "";
-}
+    ten = ""; }
 
 DoiTuong::DoiTuong(string ma, string ten) {
     this->ma = ma;
-    this->ten = ten;
-}
+    this->ten = ten; }
 
 DoiTuong::~DoiTuong() {}
+
+string DoiTuong::getMa() { return ma; }
+
+string DoiTuong::getTen() { return ten; }
+
+void DoiTuong::setMa(string m) { ma = m; }
+
+void DoiTuong::setTen(string t) { ten = t; }
--- A50812-A51090/DoiTuong.h
+++ A50812-A51090/DoiTuong.h
@@ -1,26 +1,22 @@
 #ifndef DOITUONG_H
 #define DOITUONG_H
-
 #include <string>
-#include <fstream>
 using namespace std;
-class DoiTuong {
+class DoiTuong { // tao lop co so
 protected:
     string ma;
     string ten;
-
 public:
     DoiTuong();
     DoiTuong(string ma, string ten);
     virtual ~DoiTuong();
     
-    virtual void nhap() = 0;
-    virtual void xuat() const = 0;
+    virtual void nhap() = 0; // phuong thuc thuan ao
+    virtual void xuat() = 0;
     
-    string getMa() const { return ma; }
-    string getTen() const { return ten; }
-    void setMa(string m) { ma = m; }
-    void setTen(string t) { ten = t; }
+    string getMa(); // tinh dong goi
+    string getTen();
+    void setMa(string m);
+    void setTen(string t);
 };
-
 #endif
